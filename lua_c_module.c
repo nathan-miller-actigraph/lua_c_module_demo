9eae3a4c158297610143ef991e46ca06646d659f
Nathan Miller <nathan.miller@theactigraph.com>
2018-01-10 16:12:16 -0600

    Added lua C module C file

---
+++ lua_c_module.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "lua.h"
+#include "lauxlib.h"
+
+static int _internal_get_random_number(void)
+{
+    return 4; // chosen by fair dice roll.
+              // guaranteed to be random.
+}
+
+int get_random_number(lua_State *L)
+{
+    int r = _internal_get_random_number();
+    lua_pushnumber(L, r);
+
+    return 1;
+}
+
+int luaopen_lua_c_module(lua_State *L)
+{
+    const char *msg = "hello world";
+
+    luaL_Reg fs[] = {
+        { "get_random_number", get_random_number },
+        { NULL, NULL },
+    };
+
+    lua_newtable(L);
+    lua_pushlstring(L, msg, strlen(msg));
+    lua_setfield(L, 2, "hello");
+
+    luaL_Reg *reg = fs;
+    for (; reg->name != NULL; reg++)
+    {
+        lua_pushcfunction(L, reg->func);
+        lua_setfield(L, 2, reg->name);
+    }
+
+    return 1;
+}
